633daebdb0cbc6d7a2568acca876b6c2f7d47e52
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-02-01 17:36:30 +0000

    Rewrite script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,13 @@
 # sets up the web servers for the deployment of web_static
 
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Ablordey Morgan test" | sudo tee /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
+echo "Ablordey Morgan Test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo chown -R ubuntu:ubuntu /data/
+link_file="/data/web_static/current"
+config="/etc/nginx/sites-enabled/default"
+sudo sed -i "41i \\\\tlocation /hbnb_static/ {\n\t\talias $link_file/;\n\t\tautoindex off;\n\t}\n" "$config"
+sudo service nginx restart
