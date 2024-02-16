3b587bb8304048c61e2a34ec6e771f741f939e6f
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-02-16 08:23:43 +0000

    add a default value to variable rule

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Returns a warm greeting"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns the string 'hbnb'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def print_c_and_text(text):
+    """Returns the letter C followed by the text"""
+    return f"C {escape(text).replace('_', ' ')}"
+
+
+@app.route("/python/", strict_slashes=False, defaults={"text": "is cool"})
+@app.route("/python/<text>", strict_slashes=False)
+def print_python_and_text(text):
+    """Returns Python followed by a text with a default value"""
+    return f"Python {escape(text).replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=5000)
